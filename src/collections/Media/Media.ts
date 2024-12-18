9ab2ffc6654b7ad3f475c110d15528ad3be634d1
JosueLopezRamirez <53249852+JosueLopezRamirez@users.noreply.github.com>
2024-12-17 20:24:17 -0600

    Update Media.ts

--- src/collections/Media/Media.ts
+++ src/collections/Media/Media.ts
@@ -13,7 +13,7 @@ export const Media: CollectionConfig = {
     mimeTypes: ['image/*','video/*'],
   },
   access: {
-    read: true,
+    read: () => true,
     create: loggedIn,
     update: tenantAdmins,
     delete: tenantAdmins,
